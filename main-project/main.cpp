5c812ab3f75f13a268dce98049ff335f87c69915
arosv <arosv754@gmail.com>
2024-06-04 01:39:24 +0300

    Добавлен вывод общей информации о задании

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #4. GIT\n";
+    cout << "Variant #1. Library Subscription\n";
+    cout << "Author: Yarosh Vadim\n";
     return 0;
 }
