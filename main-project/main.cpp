f1135e1ca9051caaa60d4965d16cd34d70799978
Vadim Yarosh <arosv754@gmail.com>
2024-06-04 10:37:31 +0300

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #4. GIT\n";
     cout << "Variant #1. Library Subscription\n";
     cout << "Author: Yarosh Vadim\n";
+    cout << "Group: 23PI1d\n";
     return 0;
 }
