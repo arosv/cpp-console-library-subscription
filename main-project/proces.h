152f2e34317199ec4097a2b345a4a04c7d0e1e03
Vadim Yarosh <arosv754@gmail.com>
2024-06-06 00:24:15 +0300

    Юнит тест начало

--- main-project/proces.h
+++ main-project/proces.h
@@ -1,1 +1,12 @@
-#pragma once
+// pch.h: This is a precompiled header file.
+// Files listed below are compiled only once, improving build performance for future builds.
+// This also affects IntelliSense performance, including code completion and many code browsing features.
+// However, files listed here are ALL re-compiled if any one of them is updated between builds.
+// Do not add files here that you will be updating frequently as this negates the performance advantage.
+
+#ifndef Proces_H
+#define Proces_H
+
+// add headers that you want to pre-compile here
+
+#endif //Proces_H
---
+++ unit-test-projec/unit-test-projec.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace unittestprojec
+{
+	TEST_CLASS(unittestprojec)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
