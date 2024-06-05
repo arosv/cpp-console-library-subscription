697788f04d5a04ed410b3b622e5c96d404e2fa61
Vadim Yarosh <arosv754@gmail.com>
2024-06-06 01:33:00 +0300

    Юнит тест начало

--- unit-test-projec/unit-test-projec.cpp
+++ unit-test-projec/unit-test-projec.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../main-project/book_subscription.h"
+#include "../main-project/proces.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
